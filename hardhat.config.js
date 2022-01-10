921945d18b6f8cff7d72600bc728d8a8518d956b
okdonga <okdonga@gmail.com>
2022-01-10 16:12:14 +0900

    Update solidity compiler version

--- hardhat.config.js
+++ hardhat.config.js
@@ -6,5 +6,42 @@ require("@nomiclabs/hardhat-waffle");
 require("./tasks/faucet");
 
 module.exports = {
-  solidity: "0.7.3"
+  // solidity: "0.7.3",
+  solidity: "0.8.0",
+  networks: {
+    hardhat: {
+      chainId: 1337, // default: 31337 - metamask chain id issue - https://hardhat.org/metamask-issue.html
+    },
+    localhost: {
+      url: "http://localhost:8545",
+      chainId: 1337,
+    },
+  },
+  rinkeby: {
+    chainId: 4,
+    url: process.env.ALCHEMY_RINKEBY_RPC_URL,
+    accounts: [process.env.RINKEBY_PRIVATE_KEY],
+  },
+  kovan: {
+    chainId: 42,
+    url: process.env.ALCHEMY_KOVAN_RPC_URL,
+    accounts: [process.env.KOVAN_PRIVATE_KEY],
+    saveDeployments: true,
+  },
+  mumbai: {
+    // polygon testnet
+    chainId: 80001,
+    url: process.env.ALCHEMY_MUMBAI_RPC_URL,
+    accounts: [process.env.MUMBAI_PRIVATE_KEY],
+  },
+  polygon: {
+    chainId: 137,
+    url: process.env.ALCHEMY_POLYGON_RPC_URL,
+    accounts: [process.env.POLYGON_PRIVATE_KEY],
+  },
+  ethereum: {
+    chainId: 1,
+    url: process.env.ALCHEMY_RINKEBY_RPC_URL,
+    accounts: [process.env.RINKEBY_PRIVATE_KEY],
+  },
 };

3a1bf514caef5886c367126acbf5e857a746f690
okdonga <okdonga@gmail.com>
2022-01-22 20:56:58 +0900

    Comment out an unused component

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,16 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { Dapp } from "./components/Dapp";
+// import { Dapp } from "./components/Dapp";
 import DietTracker from "./components/Diet";
 
 import "./index.css";
+// import "bootstrap/dist/css/bootstrap.css";
 
 // This is the entry point of your application, but it just renders the Dapp
 // react component. All of the logic is contained in it.
 
 ReactDOM.render(
   <React.StrictMode>
-    <Dapp />
+    {/* <Dapp /> */}
     <DietTracker />
   </React.StrictMode>,
   document.getElementById("root")
